c816f5596e75a358f2a4e7bb4e75229a0c96e0e8
joltzz <guijsilva02@gmail.com>
2022-03-30 10:20:52 -0300

    testes finais na tela de Login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,25 +1,57 @@
 import styled from "styled-components";
-// import axios from "axios";
-import {useState,} from "react";
-import {Link,} from "react-router-dom";
+import axios from "axios";
+import {useState,useContext} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import { createContext } from "react";
 
 import Input from "../Styled/Input";
 import Button from "../Styled/Button";
 
 import logo from "../../assets/images/logo.png"
-import loading from "../../assets/images/loading.svg"
+import loading from "../../assets/images/loading.svg";
 
 
 function Login([enabled, setEnabled]){
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword]= useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword]= useState('');
+
+    const {setUser, setToken}=useContext(userContext);
+    const userContext = createContext();
+
+    const navigate=useNavigate();
+    
 
     function handleLogin(e){
         e.preventDefault();
     }
 
-    //fazer a veficiação do login
+    if(!email || !password){
+        alert('Preencha os dados corretamente e tente novamente!')
+    }
+    else{
+        setEnabled(false)
+
+        const promise=axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login",{
+            email,
+            password
+        });
+
+        promise.then(response=>{
+            setEnabled(true);
+            setUser(response.data);
+            setToken(response.data.token);
+            navigate('/hoje');
+        })
+
+        promise.catch(err=>{
+            alert('Email ou senha inválido(s)! Tente novamente!');
+            setEnabled(true);
+            setEmail('');
+            setPassword('');
+        })
+    }
+    
 
     return (
         <>
@@ -60,36 +92,33 @@ function Login([enabled, setEnabled]){
 
 export default Login;
 
-const Container=styled.div`
-
-    width: 100%
-    height 100vh;
+const Container = styled.div`
+    width: 100%;
+    height: 667px;
     padding: 68px 36px 0 36px;
     background-color: #fff;
     display: flex;
     flex-direction: column;
     align-items: center;
-
-    img{
+    
+    img {
         width: 180px;
-        height: 180px;
+        height: 179px;
         margin-bottom: 32px;
-    }
+    } 
 `
 
-const FormLogin=styled.div`
-
+const FormLogin = styled.div`
     display:flex;
     flex-direction: column;
     align-items: center;
     height: auto;
 `
 
-const StyledLink=styled(Link)`
-
-    margin-top:25px;
-    font-size: 14px
+const StyledLink = styled(Link)`
+    margin-top: 25px;
+    font-size: 14px;
+    font-weight: 400;
     text-align: center;
     color: #52B6FF;
 `
-
