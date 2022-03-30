bebce8456d1fbb6b78c9361df04b5fc58f57b45d
joltzz <guijsilva02@gmail.com>
2022-03-30 20:16:44 -0300

    adicionado mais alguns componentes e iniciando a tela /hoje

--- src/components/Habits/index.js
+++ src/components/Habits/index.js
@@ -2,6 +2,18 @@ import axios from "axios";
 import { useState, useEffect, useContext } from "react";
 import styled from "styled-components";
 
+import TopBar from "../TopBar";
+import Menu from "../Menu";
+import DayButton from "../DayButton";
+import HabitsDays from "../HabitsDays";
+import UserContext from "../Context/UserContext";
 
+import trash from "../../assets/images/Trashcan.svg"
 
-
+function Habits(){
+    return (
+        <>
+        
+        </>
+    )
+}
---
+++ src/components/HabitsDays/index.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+
+function HabitsDays(){
+    
+    let selected=false;
+
+    if(daysToDo.includes(days.id)){
+        selected=true;
+    }
+
+    return(
+        <>
+            <daysToDo selected={selected}>
+                {days.name}
+            </daysToDo>
+        </>
+    )
+}
+
+export default HabitsDays;
+
+
+const daysToDo=styled.div`
+
+    width: 30px;
+    height:30px;
+    display:flex;
+    justify-content:center;
+    align-items: center;
+    border: 1px solid #d4d4d4;
+    border-radius: 5px
+    margin: 8px 4px 0 0;
+    font-size: 17px;
+
+    color: ${({selected})=> selected ? "#FFF" : "#dbdbdb"};
+    background-color: ${({selected})=> selected ? "#dbdbdb" : "#FFF"};
+
+`
