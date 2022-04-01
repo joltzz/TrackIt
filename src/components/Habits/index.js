e0e61718039f7a06a0c43e0d4bc12de3987e5581
joltzz <guijsilva02@gmail.com>
2022-04-01 10:16:43 -0300

    adicionado styled components na tela de habitos

--- src/components/Habits/index.js
+++ src/components/Habits/index.js
@@ -203,4 +203,117 @@ function Habits() {
     )
 }
 
+const Container=styled.div`
+    padding-bottom: 90px;
+`
+const Content=styled.div`
+    padding: 22px 17px;
+    height: 100;
+
+    span{
+        font-size:18px;
+        color: #666666
+    }
+`
+const NavBar=styled.div`
+    color: #52B6FF;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 22px
+
+    h1{
+        font-size: 23px
+    }
+`
+const AddHabit=styled.button`
+    width: 40px;
+    height:35px;
+    border-radius: 4.6px;
+    background-color: #52B6FF;
+    font-size: 27px;
+    color: #FFF;
+`
+const CreateHabit=styled.div`
+    width: 100%;
+    height: 180px;
+    margin-bottom: 29px;
+    padding: 18px 18px 15px 18px;
+    background-color: #fff;
+    border-radius: 5px;
+
+    .buttons{
+        margin-top: 32px;
+        display: flex;
+        justify-content: flex-end;
+    }
+    input{
+        width:303px;
+        height: 45px;
+        padding: 11px;
+        font-size: 20px;
+        border: 1px solid #d4d4d4;
+        border-radius: 5px;
+
+        ::placeholder{
+            color: #dbdbdb;
+        }
+        ::disabled{
+            background-color: #f2f2f2;
+        }
+    }
+`
+const DaySelect=styled.div`
+    display: flex;
+`
+const SaveButton=styled.button`
+    width: 84px;
+    height: 35px;
+    background-color: #52B6FF;
+    color: #fff;
+    border-radius: 5px;
+
+    :disabled {
+        opacity: 0.7;
+    }
+`
+
+const CancelButton = styled.button`
+    width: 84px;
+    height: 35px;
+    background: none;
+    color: #52B6FF;
+
+    :disabled {
+        opacity: 0.7;
+    }
+`
+
+const HabitsList = styled.div`
+    width: 100%;
+    height: auto;
+`
+
+const EachHabit = styled.div`
+    width: 100%;
+    height: auto;
+    margin-bottom: 10px;
+    border-radius: 5px;
+    padding: 13px 11px 15px 15px;
+    background-color: #fff;
+    position: relative;
+
+    span {
+        font-size: 20px;
+    }
+    img {
+        position: absolute;
+        top: 11px;
+        right: 10px;
+    }
+    .days {
+        display: flex;
+    }
+`
+
 export default Habits;
